68d8ba83f620376ace1da1ed209524690c753b8c
aditya mahes <aditya.maheswara.wibowo@gmail.com>
2026-04-09 21:22:45 +0700

    Delete pass or fail.py

--- pass or fail.py
+++
@@ -1,9 +0,0 @@
-Title:Create a flowchart and pseudocode to determine student pass/fail.
-Declaration: nilai
-implementation: 
-if nilai >= 75 
-	display "lulus"
-if nilai < 75
-	display "tidak lulus"
-
-
