bf15a4bc38754d8c832b38f0c8662f3270551bcb
aditya mahes <aditya.maheswara.wibowo@gmail.com>
2026-04-09 21:17:38 +0700

    Add files via upload

---
+++ area of footbal pseudo.py
@@ -0,0 +1,5 @@
+Title: Calculate the area of a football field
+Declaration: Panjang, lebar
+implementation:
+area = panjang*lebar
+display area
---
+++ pass or fail.py
@@ -0,0 +1,9 @@
+Title:Create a flowchart and pseudocode to determine student pass/fail.
+Declaration: nilai
+implementation: 
+if nilai >= 75 
+	display "lulus"
+if nilai < 75
+	display "tidak lulus"
+
+
